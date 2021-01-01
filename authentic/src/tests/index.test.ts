bd9f8f808b2840a34a1e9e55b9d6a839fdd6d304
Ashley Davis <ashley@codecapers.com.au>
2021-01-02 07:57:50 +1000

    Refactored tests.

--- authentic/src/tests/index.test.ts
+++ authentic/src/tests/index.test.ts
@@ -5,46 +5,64 @@ const baseUrl = "http://localhost:3000";
 
 describe("authentic", () => {
 
-    it("can register and then confirm new user", async () => { //TODO: break this up into two tests with helper fns.
+    //
+    // Registers a new user and returns their confirmation token.
+    //
+    async function registerNewUser() {
+        const mockConsole = jest.spyOn(global.console, 'log');
+    
+        const registerResponse = await axios.post(`${baseUrl}/api/auth/register`, {
+            "email": "someone@something.com",
+            "password": "fooey"
+        });
+    
+        expect(registerResponse.status).toBe(200);
+    
+        let output = "";
+    
+        for (const call of mockConsole.mock.calls) {
+            for (const param of call) {
+                output += param.toString();
+            }
+        }
+    
+        const match = /signup-confirmed\/(.*)\?/g.exec(output);
+    
+        const confirmationToken = match![1];
+        console.log(confirmationToken);
+
+        mockConsole.mockRestore(); //todo: do I need this?
+        mockConsole.mockClear();
+
+        return confirmationToken;
+    }
+    
+    //
+    // Confirms a newly registered user.
+    //
+    async function confirmNewUser(confirmationToken: string) {
+        const confirmResponse = await axios.post(`${baseUrl}/api/auth/confirm`, {
+            "email": "someone@something.com",
+            "token": confirmationToken
+        });
+    
+        expect(confirmResponse.status).toBe(200);
+    }
+    
+    
+    it("can register and then confirm new user", async () => {
 
         const microservice = await main();
 
         const usersCollection = microservice.db.collection("users");
         await usersCollection.drop();
 
-        const mockConsole = jest.spyOn(global.console, 'log');
+        const confirmationToken = await registerNewUser();
 
-        const registerResponse = await axios.post(`${baseUrl}/api/auth/register`, {
-            "email": "someone@something.com",
-            "password": "fooey"
-        });
-
-        expect(registerResponse.status).toBe(200);
-
-        let output = "";
-
-        for (const call of mockConsole.mock.calls) {
-            for (const param of call) {
-                output += param.toString();
-            }
-        }
-
-        const match = /signup-confirmed\/(.*)\?/g.exec(output);
-
-        const confirmationToken = match![1];
-        console.log(confirmationToken);
-
-        const confirmResponse = await axios.post(`${baseUrl}/api/auth/confirm`, {
-            "email": "someone@something.com",
-            "token": confirmationToken
-        });
-
-        expect(confirmResponse.status).toBe(200);
+        await confirmNewUser(confirmationToken);
 
         await microservice.stop();
 
-        mockConsole.mockRestore(); //todo: do I need this?
-        mockConsole.mockClear();
     });
 
     //todo:
@@ -53,3 +71,4 @@ describe("authentic", () => {
     // must provide valid password
     // can't register user more than once
 });
+
