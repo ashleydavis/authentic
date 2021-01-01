b725c99a4367b1068006dd96c84b575cf755dc37
Ashley Davis <ashley@codecapers.com.au>
2021-01-01 11:18:13 +1000

    Moved some helper function to global scope.

--- authentic/src/index.ts
+++ authentic/src/index.ts
@@ -187,60 +187,6 @@ async function main() {
         });
     });
 
-    //
-    // Helper for HTTP POST with async handler.
-    //
-    function post(app: express.Router, route: string, handler: (req: express.Request, res: express.Response) => Promise<void>) {
-        console.log("Registering HTTP POST " + route);
-        app.post(route, (req, res) => {
-            console.log("Handling HTTP POST " + route);
-            handler(req,  res)
-                .catch(err => {
-                    console.error("Error from handler: HTTP POST " + route);
-                    console.error(err && err.stack || err);
-
-                    res.sendStatus(500);
-                });
-        });
-    }
-
-    //
-    // Helper for HTTP GET with async handler.
-    //
-    function get(app: express.Router, route: string, handler: (req: express.Request, res: express.Response) => Promise<void>) {
-        console.log("Registering HTTP GET " + route);
-        app.get(route, (req, res) => {
-            handler(req,  res)
-                .catch(err => {
-                    console.error("Error from handler: HTTP GET " + route);
-                    console.error(err && err.stack || err);
-
-                    res.sendStatus(500);
-                });
-        });
-    }
-
-    //
-    // Verify that a query parameter to a request is set.
-    //
-    function verifyQueryParam(name: string, req: any): string {
-        if (!req.query[name]) {
-            throw new Error("Missing query parameter " + name);
-        }
-
-        return req.query[name];
-    }
-
-    //
-    // Verify that a body parameter to a request is set.
-    //
-    function verifyBodyParam(name: string, req: any): string {
-        if (!req.body[name]) {
-            throw new Error("Missing body parameter " + name);
-        }
-
-        return req.body[name];
-    }    
     /*
     HTTP POST /auth/register
     BODY
@@ -758,3 +704,57 @@ export function authenticateJWT(req: Express.Request, res: express.Response, don
     });
 }
 
+//
+// Helper for HTTP POST with async handler.
+//
+function post(app: express.Router, route: string, handler: (req: express.Request, res: express.Response) => Promise<void>) {
+    console.log("Registering HTTP POST " + route);
+    app.post(route, (req, res) => {
+        console.log("Handling HTTP POST " + route);
+        handler(req,  res)
+            .catch(err => {
+                console.error("Error from handler: HTTP POST " + route);
+                console.error(err && err.stack || err);
+
+                res.sendStatus(500);
+            });
+    });
+}
+
+//
+// Helper for HTTP GET with async handler.
+//
+function get(app: express.Router, route: string, handler: (req: express.Request, res: express.Response) => Promise<void>) {
+    console.log("Registering HTTP GET " + route);
+    app.get(route, (req, res) => {
+        handler(req,  res)
+            .catch(err => {
+                console.error("Error from handler: HTTP GET " + route);
+                console.error(err && err.stack || err);
+
+                res.sendStatus(500);
+            });
+    });
+}
+
+//
+// Verify that a query parameter to a request is set.
+//
+function verifyQueryParam(name: string, req: any): string {
+    if (!req.query[name]) {
+        throw new Error("Missing query parameter " + name);
+    }
+
+    return req.query[name];
+}
+
+//
+// Verify that a body parameter to a request is set.
+//
+function verifyBodyParam(name: string, req: any): string {
+    if (!req.body[name]) {
+        throw new Error("Missing body parameter " + name);
+    }
+
+    return req.body[name];
+}    
